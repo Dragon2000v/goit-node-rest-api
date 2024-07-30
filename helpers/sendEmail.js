606e666e5285a294be48f4750034c8d02fd6c3bd
Dragon2000v <shmatko064@gmail.com>
2024-07-30 17:33:22 +0300

    add nodemail

---
+++ helpers/sendEmail.js
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+import { env } from './env.js';
+dotenv.config();
+const VERIFIED_MAIL = env('VERIFIED_MAIL');
+const VERIFIED_MAIL_PASSWORD = env('VERIFIED_MAIL_PASSWORD');
+
+const nodemailerConfig = {
+  host: 'smtp.ukr.net',
+  port: 465,
+  secure: true,
+  auth: {
+    user: VERIFIED_MAIL,
+    pass: VERIFIED_MAIL_PASSWORD,
+  },
+};
+const transporter = nodemailer.createTransport(nodemailerConfig);
+
+export const sendMail = (data) => {
+  const email = { ...data, from: VERIFIED_MAIL };
+  transporter.sendMail(email);
+};
